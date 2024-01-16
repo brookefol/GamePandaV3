e652c31e67ab4c46d8944d19db61aa4cda29dd37
brookefol <bfoley2026@gmail.com>
2024-01-16 14:52:17 -0500

    Change game screen works!

--- start_menu.gd
+++ start_menu.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_start_button_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://choose_game.tscn")# Replace with function body.
 	
 
 
