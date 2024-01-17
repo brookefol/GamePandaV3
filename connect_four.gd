56a92e9ebcd131d39437af6c6792354a8b304d64
bomiejun <116047701+bomiejun@users.noreply.github.com>
2024-01-17 12:27:24 -0500

    ahweas9gudfimmmm

--- connect_four.gd
+++ connect_four.gd
@@ -3,52 +3,9 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# each list is a column
-	var boardArray = [
-		[0, 0, 0, 0, 0, 0], 
-		[0, 0, 0, 0, 0, 0], 
-		[0, 0, 0, 0, 0, 0], 
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0], 
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0], 
-		[0, 0, 0, 0, 0, 0]]
-	
-	
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _column1_add():
-	pass # Replace with function body.
-
-
-func _column2_add():
-	pass # Replace with function body.
-
-
-func _column3_add():
-	pass # Replace with function body.
-
-
-func _column4_add():
-	pass # Replace with function body.
-
-
-func _column5_add():
-	pass # Replace with function body.
-
-
-func _column6_add():
-	pass # Replace with function body.
-
-
-func _column7_add():
-	pass # Replace with function body.
-
-
-func _column8_add():
-	pass # Replace with function body.
