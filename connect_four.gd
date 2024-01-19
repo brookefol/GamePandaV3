462bb0fc2f1d12718b7666a5a6fbc4058840f9b6
bomiejun <116047701+bomiejun@users.noreply.github.com>
2024-01-19 10:23:25 -0500

    checkWin function added to connect four

--- connect_four.gd
+++ connect_four.gd
@@ -33,11 +33,39 @@ func _setChipsOff():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func _winCheck():
+	print(board[5], "\n", board[4], "\n", board[3], "\n",  board[2], "\n", board[1], "\n", board[0])
+	# horizontal
+	for i in range(6):
+		for j in range(5):
+			if board[i][j] == playerSymbol[(playerTurn+1)%2] and board[i][j] == board[i][j+1] and board[i][j+1] == board[i][j+2] and board[i][j+2] == board[i][j+3]:
+				var tempWin = get_node("tempWin")
+				tempWin.visible = true
+				break
+	# verticle
+	for i in range(8):
+		for j in range(3):
+			if board[j][i] == playerSymbol[(playerTurn+1)%2] and board[j][i] == board[j+1][i] and board[j+1][i] == board[j+2][i] and board[j+2][i] == board[j+3][i]:
+				var tempWin = get_node("tempWin")
+				tempWin.visible = true
+				break
+	# slants
+	for i in range(5):
+		for j in range(3):
+			if board[j][i] == playerSymbol[(playerTurn+1)%2] and board[j][i] == board[j+1][i+1] and board[j+1][i+1] == board[j+2][i+2] and board[j+2][i+2] == board[j+3][i+3]:
+				var tempWin = get_node("tempWin")
+				tempWin.visible = true
+				break
+			if board[j+3][i] == playerSymbol[(playerTurn+1)%2] and board[j+3][i] == board[j+2][i+1] and board[j+2][i+1] == board[j+1][i+2] and board[j+1][i+2] == board[j][i+3]:
+				var tempWin = get_node("tempWin")
+				tempWin.visible = true
+				break
 
 
 func _column0_add():
 	for i in range(6):
-		if board[5-i][0] == null:
+		if board[i][0] == null:
 			var value = str(5-i) + str(0)
 			if playerTurn % 2 == 0:
 				var button = get_node("GridContainer/"+value)
@@ -45,16 +73,17 @@ func _column0_add():
 					button.disabled = false
 			else:
 				var button = get_node("GridContainer2/"+value)
-				board[5-i][0] = playerSymbol[playerTurn%2]
 				if button:
 					button.disabled = false
+			board[i][0] = playerSymbol[playerTurn%2]
 			break
 	playerTurn += 1
+	_winCheck()
 
 
 func _column1_add():
 	for i in range(6):
-		if board[5-i][1] == null:
+		if board[i][1] == null:
 			var value = str(5-i) + str(1)
 			if playerTurn % 2 == 0:
 				var button = get_node("GridContainer/"+value)
@@ -64,14 +93,15 @@ func _column1_add():
 				var button = get_node("GridContainer2/"+value)
 				if button:
 					button.disabled = false
-			board[5-i][1] = playerSymbol[playerTurn%2]
+			board[i][1] = playerSymbol[playerTurn%2]
 			break
 	playerTurn += 1
+	_winCheck()
 
 
 func _column2_add():
 	for i in range(6):
-		if board[5-i][2] == null:
+		if board[i][2] == null:
 			var value = str(5-i) + str(2)
 			if playerTurn % 2 == 0:
 				var button = get_node("GridContainer/"+value)
@@ -81,14 +111,15 @@ func _column2_add():
 				var button = get_node("GridContainer2/"+value)
 				if button:
 					button.disabled = false
-			board[5-i][2] = playerSymbol[playerTurn%2]
+			board[i][2] = playerSymbol[playerTurn%2]
 			break
 	playerTurn += 1
+	_winCheck()
 
 
 func _column3_add():
 	for i in range(6):
-		if board[5-i][3] == null:
+		if board[i][3] == null:
 			var value = str(5-i) + str(3)
 			if playerTurn % 2 == 0:
 				var button = get_node("GridContainer/"+value)
@@ -98,14 +129,15 @@ func _column3_add():
 				var button = get_node("GridContainer2/"+value)
 				if button:
 					button.disabled = false
-			board[5-i][3] = playerSymbol[playerTurn%2]
+			board[i][3] = playerSymbol[playerTurn%2]
 			break
 	playerTurn += 1
+	_winCheck()
 
 
 func _column4_add():
 	for i in range(6):
-		if board[5-i][4] == null:
+		if board[i][4] == null:
 			var value = str(5-i) + str(4)
 			if playerTurn % 2 == 0:
 				var button = get_node("GridContainer/"+value)
@@ -115,14 +147,15 @@ func _column4_add():
 				var button = get_node("GridContainer2/"+value)
 				if button:
 					button.disabled = false
-			board[5-i][4] = playerSymbol[playerTurn%2]
+			board[i][4] = playerSymbol[playerTurn%2]
 			break
 	playerTurn += 1
+	_winCheck()
 
 
 func _column5_add():
 	for i in range(6):
-		if board[5-i][5] == null:
+		if board[i][5] == null:
 			var value = str(5-i) + str(5)
 			if playerTurn % 2 == 0:
 				var button = get_node("GridContainer/"+value)
@@ -132,14 +165,15 @@ func _column5_add():
 				var button = get_node("GridContainer2/"+value)
 				if button:
 					button.disabled = false
-			board[5-i][5] = playerSymbol[playerTurn%2]
+			board[i][5] = playerSymbol[playerTurn%2]
 			break
 	playerTurn += 1
+	_winCheck()
 
 
 func _column6_add():
 	for i in range(6):
-		if board[5-i][6] == null:
+		if board[i][6] == null:
 			var value = str(5-i) + str(6)
 			if playerTurn % 2 == 0:
 				var button = get_node("GridContainer/"+value)
@@ -149,14 +183,15 @@ func _column6_add():
 				var button = get_node("GridContainer2/"+value)
 				if button:
 					button.disabled = false
-			board[5-i][6] = playerSymbol[playerTurn%2]
+			board[i][6] = playerSymbol[playerTurn%2]
 			break
 	playerTurn += 1
+	_winCheck()
 
 
 func _column7_add():
 	for i in range(6):
-		if board[5-i][7] == null:
+		if board[i][7] == null:
 			var value = str(5-i) + str(7)
 			if playerTurn % 2 == 0:
 				var button = get_node("GridContainer/"+value)
@@ -166,13 +201,11 @@ func _column7_add():
 				var button = get_node("GridContainer2/"+value)
 				if button:
 					button.disabled = false
-			board[5-i][7] = playerSymbol[playerTurn%2]
+			board[i][7] = playerSymbol[playerTurn%2]
 			break
 	playerTurn += 1
+	_winCheck()
 
-
-func _on_column_5_pressed():
-	pass # Replace with function body.
 
 
 func _on_back_button_pressed():
