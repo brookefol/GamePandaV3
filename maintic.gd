7b0e3d5e2dae6a8cb52adcc55ebd25f0c0f6c7ba
brookefol <bfoley2026@gmail.com>
2024-01-30 23:06:12 -0500

    tictactoe!

--- maintic.gd
+++ maintic.gd
@@ -1,5 +1,6 @@
 extends Node
 
+var grid_data: Array
 var grid_pos: Vector2i # creates (x,y) as variable
 var board_size: int
 var cell_size: int
@@ -9,6 +10,7 @@ func _ready():
 	board_size = $Board.texture.get_width() # Replace with function body.
 	# board size divided by 3 to split boxes to get individual cells
 	cell_size = board_size/3
+	new_game()
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,5 +21,13 @@ func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			if event.position.x < board_size:
-				grid_pos = Vector2i(event.position/cell_size)
+				grid_pos = Vector2i(event.position/cell_size) # turning mouse position into coordinates for each of the 9 boxes
 				print(grid_pos)
+				
+
+func new_game():
+	grid_data = [
+		[0, 0, 0], 
+		[0, 0, 0], 
+		[0, 0, 0]
+		] # three rows of three
