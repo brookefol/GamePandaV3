3df55ddc06853a0fefce511aa8ce5e5e04e2b8d0
brookefol <bfoley2026@gmail.com>
2024-01-30 23:02:38 -0500

    tictactoe in process

--- maintic.gd
+++ maintic.gd
@@ -1,20 +1,15 @@
-extends Node2D
+extends Node
 
-var grid_pos : Vector2i 
+var grid_pos: Vector2i # creates (x,y) as variable
 var board_size: int
 var cell_size: int
-
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	board_size = $Board.texture.get_width() # Replace with function body.
-	# divide board size by 3 to get size of individual cells
-	cell_size = board_size / 3
+	# board size divided by 3 to split boxes to get individual cells
+	cell_size = board_size/3
 	
-	
-	
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -23,8 +18,6 @@ func _process(delta):
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-	
 			if event.position.x < board_size:
-				# convert mouse position into grid position
-				grid_pos = Vector2i((event.position)/(cell_size))
+				grid_pos = Vector2i(event.position/cell_size)
 				print(grid_pos)
